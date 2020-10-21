c999a9c8c7944d4c7c017fb1eed558d1c4d91160
John Suico <johncdsuico@gmail.com>
2020-10-21 08:01:36 -0700

    initialize dashboard, sidebar, and css

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,24 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from 'react-router-dom';
 import './App.css';
+
+// Components
+import Dashboard from './components/Dashboard/Dashbord';
 
 function App() {
   return (
     <div className="App">
-      <h1>React App Homepage</h1>
+      <Router>
+        <Switch>
+          <Route exact path='/'>
+            <Dashboard />
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
---
+++ client/src/components/Dashboard/Dashbord.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Axios from 'axios';
+
+function Dashboard() {
+  return (
+    <div className="dashBoard">
+      <h1>Dashboard Component</h1>
+
+    </div>
+  )
+}
+
+export default Dashboard;
