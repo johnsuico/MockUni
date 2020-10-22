267c9f87e4fdacc8c23af855a5177664d56978ef
John Suico <johncdsuico@gmail.com>
2020-10-22 13:20:50 -0700

    Finish select affects

--- client/src/components/Students/Students.js
+++ client/src/components/Students/Students.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import Axios from 'axios';
 
 // Import CSS File
 import './students.css';
@@ -7,8 +8,20 @@ import './student-sideOne.css';
 // Import Components
 import Sidebar from '../Sidebar/Sidebar';
 import SideOne from './SubComponents/StudentSideOne';
+import SideTwo from './SubComponents/StudentSideTwo';
 
 function Students() {
+
+  const [selected, setSelected] = useState('');
+
+  useEffect(() => {
+    Axios.get('http://localhost:5000/students')
+      .then(response => setSelected(response.data[0].SID))
+  }, [])
+
+  function getSelected(selected) {
+    setSelected(selected);
+  }
 
   return (
     <div className="students">
@@ -20,7 +33,7 @@ function Students() {
           <div className="sideOne-header">
             <h2>Student Management</h2>
           </div>
-          <SideOne />
+          <SideOne getSelected={getSelected}/>
         </div>
 
         {/*  Students Side Two */}
@@ -28,6 +41,7 @@ function Students() {
           <div className="sideTwo-header">
             <h2>Mock University Name</h2>
           </div>
+          <SideTwo selected={selected}/>
         </div>
       </div>
     </div>
--- client/src/components/Students/SubComponents/TableRow.js
+++ client/src/components/Students/SubComponents/TableRow.js
@@ -1,8 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function TableRow(props) {
+
+  const [SID, setSID] = useState(props.SID);
+
+  function onClick() {
+    props.onClickParent(SID)
+  }
+
   return (
-    <tr className="tableRow">
+    <tr className={props.selected === props.SID ? "tableRow-selected" : "tableRow"} onClick={onClick}>
       <td className="tableData">{props.index}</td>
       <td className="tableData">{props.firstName}</td>
       <td className="tableData">{props.lastName}</td>
