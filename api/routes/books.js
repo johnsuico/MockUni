0f7e72162bfaf52700acdf71181d019b61306c4d
John Suico <johncdsuico@gmail.com>
2020-12-01 08:20:53 -0800

    reformat code structure for docker use

--- api/routes/books.js
+++ api/routes/books.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const Book = require('../models/book');
+const Student = require('../models/student');
 
 // ROUTE:   /books/
 // DESC :   Get all books
@@ -18,25 +19,6 @@ router.route('/').delete((req, res) => {
         .catch(err => res.send(err));
 })
 
-// ROUTE:   /books/:id
-// DESC :   Update a book
-// REQ  :   PUT
-router.route('/:id').put((req, res) => {
-    const id = req.params.id;
-    const { title, author, ISBN } = req.body;
-    
-    const update = {
-        title, author, ISBN
-    }
-    
-    console.log("Udpate submitted for book edit: ", update);
-
-    Book.findByIdAndUpdate(id, update)
-      .then(books => res.json({status: 'ok'}))
-      .catch(err => res.json(err));
-    
-  });
-  
 // ROUTE:   /books/
 // DESC :   Add a new book to database
 // REQ  :   POST
@@ -73,6 +55,23 @@ router.route('/:id').get((req, res) => {
 });
 
 // ROUTE:   /books/:id
+// DESC :   Update a book
+// REQ  :   PUT
+router.route('/:id').put((req, res) => {
+  const id = req.params.id;
+  const { title, author, ISBN } = req.body;
+  
+  const update = {
+     title, author, ISBN
+  }
+  
+  Book.findByIdAndUpdate(id, update)
+    .then(book => res.json({status: 'ok'}))
+    .catch(err => res.json(err));
+  
+});
+
+// ROUTE:   /books/:id
 // DESC :   Delete a book from the database
 // REQ  :   DELETE
 router.route('/:id').delete((req, res) => {
@@ -83,5 +82,56 @@ router.route('/:id').delete((req, res) => {
         .catch(err => res.send(err));
 });
 
+// ROUTE:   /books/:id/student
+// DESC :   Update class object to add student to array
+// REQ  :   PUT
+router.route('/:id/student').put((req, res) => {
+
+  const id = req.params.id;
+  const studentID = req.body.studentID;
+
+  Book.findByIdAndUpdate(id)
+    .then(books => {
+      books.students.push(studentID);
+      books.save();
+
+      Student.findByIdAndUpdate(studentID)
+        .then(newStudent => {
+          newStudent.classes.push(id);
+          newStudent.save();
+        })
+    })
+    .catch(err => console.log(err));
+
+  // console.log(studentID);
+});
+
+// ROUTE:   /books/:id
+// DESC :   Delete a class from the database
+// REQ  :   DELETE
+router.route('/:id').delete((req, res) => {
+  const id = req.params.id;
+
+  Book.findById(id, (err, books) => {
+    if(books) {
+      console.log('Found class');
+      books.students.map(selected => {
+        Student.findByIdAndUpdate(
+          selected,
+          { $pull : {"books": books._id}}
+        )
+        .then(console.log('Removed book from student'))
+      });
+    } else {
+      console.log(err);
+    }
+  });
+
+  Book.findByIdAndDelete(id)
+    .then(books => {
+      res.send(`Class: ${books.title} ${books.ISBN} has been deleted`);
+    })
+    .catch (err => console.log(err));
+});
 
 module.exports = router;
