57583f541ffebff26517822a5ebf07823a747e64
John Suico <johncdsuico@gmail.com>
2020-12-21 20:17:59 -0800

    Changed URL for demo

--- client/src/App.js
+++ client/src/App.js
@@ -38,7 +38,7 @@ function App() {
           <Route path="/classes/edit/:id">
             <EditClass />
           </Route>
-          <Route path="/books/edit/:id">
+          <Route path="/books/edit/:idsdfsdf">
             <EditBook />
           </Route>
         </Switch>
